0b04648975b525e621ebdaa62983f3afa449460b
ashiful-islam <ashifulislam2002@gmail.com>
2025-01-12 15:29:03 +0600

    learned tumple type data type

---
+++ User defined type/tuple-type.js
@@ -0,0 +1,7 @@
+// mixed data type
+// fixed data type dite hobe
+// key value type er data type check korte use hoy
+var userDetails;
+userDetails = [122, "string"];
+userDetails.push(211, "name 2");
+console.log(userDetails);
---
+++ User defined type/tuple-type.ts
@@ -0,0 +1,10 @@
+// mixed data type
+
+// fixed data type dite hobe
+// key value type er data type check korte use hoy
+
+let userDetails: [number, string];
+
+userDetails = [122, "string"];
+userDetails.push(211, "name 2");
+console.log(userDetails);
